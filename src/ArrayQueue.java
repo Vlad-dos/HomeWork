ebeaf46004e53156171d2edd1af685bc3b2fda7f
Vlad <vlad082006@yandex.ru>
2016-03-04 20:45:22 +0300

    modif

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,5 +1,5 @@
 public class ArrayQueue {
-    static final private int MIN_SIZE = 10;
+    final private int MIN_SIZE = 10;
     private int head = 0;
     private int tail = 0;
     private int size = 0;
@@ -13,27 +13,28 @@ public class ArrayQueue {
         if (tail > head) {
             System.arraycopy(data, head, newData, 0, tail - head);
         } else {
-            int j = 0;
-            for (int i = head; i < data.length; i++) {
-                newData[j++] = data[i];
-            }
-            for (int i = 0; i < tail; i++) {
-                newData[j++] = data[i];
-            }
+            System.arraycopy(data, head, newData, 0, data.length - head);
+            System.arraycopy(data, 0, newData, data.length - head, tail);
         }
         data = newData;
         head = 0;
         tail = size;
     }
 
+    //post: result = next i
     private int next(int i) {
         return (i + 1) % data.length;
     }
 
+    //post: result = prev i
+    private int prev(int i) {
+        return (i + data.length - 1) % data.length;
+    }
+
     // pre: a != null
     // post: size == size' + 1 &&
-    //       tail == tail' + 1 &&
-    //       data[tail'] == a
+    //       Any 0 <= i < size - 1 data[i] == data'[i]
+    //       data[size - 1] == a
     public void enqueue(Object a) {
         ensureCapacity(size + 1);
         size++;
@@ -41,36 +42,73 @@ public class ArrayQueue {
         tail = next(tail);
     }
 
+    // pre: a != null
+    // post: size == size' + 1 &&
+    //       Any 0 < i < size data[i - 1] == data'[i]
+    //       data[0] == a
+    public void push(Object a) {
+        ensureCapacity(size + 1);
+        size++;
+        head = prev(head);
+        data[head] = a;
+    }
+
     // pre: size > 0
-    // post: result == data[head]
+    // post: result == data[0]
+    //       Any 0 <= i < size data[i] == data'[i]
     public Object element() {
         return data[head];
     }
 
     // pre: size > 0
-    // post: result == data[head] && head = head' + 1
+    // post: result == data[size - 1]
+    //       Any 0 <= i < size data[i] == data'[i]
+    public Object peek() {
+        return data[prev(tail)];
+    }
+
+    // pre: size > 0
+    // post: result == data[0]
+    //       Any 0 <= i < size data[i + 1] == data'[i]
     public Object dequeue() {
         size--;
         Object tmp = data[head];
+        data[head] = null;
         head = next(head);
         return tmp;
     }
 
+    // pre: size > 0
+    // post: result == data[size - 1]
+    //       Any 0 <= i < size - 1 data[i] == data'[i]
+    public Object remove() {
+        size--;
+        tail = prev(tail);
+        Object tmp = data[tail];
+        data[tail] = null;
+        return tmp;
+    }
+
     // post: result == size
+    //       data == data';
+    //       size == size';
     public int size() {
         return size;
     }
 
     // post: result == size > 0
+    //       data == data';
     public boolean isEmpty() {
         return size == 0;
     }
 
-    // post: head = 0 && tail == 0 && size == 0
+    // post: size == 0
+    //       data == data';
+    //       size == size';
     public void clear() {
         tail = 0;
         head = 0;
         size = 0;
         data = new Object[MIN_SIZE];
-    }
-}
+    }}
+
--- src/ArrayQueueADT.java
+++ src/ArrayQueueADT.java
@@ -5,6 +5,8 @@ public class ArrayQueueADT {
     private int size = 0;
     private Object[] data = new Object[MIN_SIZE];
 
+    //pre: queue != null
+    //post data.length > capacity
     static private void ensureCapacity(ArrayQueueADT queue, int capacity) {
         if (capacity < queue.data.length) {
             return;
@@ -13,27 +15,31 @@ public class ArrayQueueADT {
         if (queue.tail > queue.head) {
             System.arraycopy(queue.data, queue.head, newData, 0, queue.tail - queue.head);
         } else {
-            int j = 0;
-            for (int i = queue.head; i < queue.data.length; i++) {
-                newData[j++] = queue.data[i];
-            }
-            for (int i = 0; i < queue.tail; i++) {
-                newData[j++] = queue.data[i];
-            }
+            System.arraycopy(queue.data, queue.head, newData, 0, queue.data.length - queue.head);
+            System.arraycopy(queue.data, 0, newData, queue.data.length - queue.head, queue.tail);
         }
         queue.data = newData;
         queue.head = 0;
         queue.tail = queue.size;
     }
 
+    //pre: queue != null
+    //post: result = next i
     static private int next(ArrayQueueADT queue, int i) {
         return (i + 1) % queue.data.length;
     }
 
+    //pre: queue != null
+    //post: result = prev i
+    static private int prev(ArrayQueueADT queue, int i) {
+        return (i + queue.data.length - 1) % queue.data.length;
+    }
+
     // pre: a != null
+    //      queue != null
     // post: size == size' + 1 &&
-    //       tail == tail' + 1 &&
-    //       data[tail'] == a
+    //       Any 0 <= i < size - 1 data[i] == data'[i]
+    //       data[size - 1] == a
     static public void enqueue(ArrayQueueADT queue, Object a) {
         ensureCapacity(queue, queue.size + 1);
         queue.size++;
@@ -41,32 +47,77 @@ public class ArrayQueueADT {
         queue.tail = next(queue, queue.tail);
     }
 
+    // pre: a != null
+    //      queue != null
+    // post: size == size' + 1 &&
+    //       Any 0 < i < size data[i - 1] == data'[i]
+    //       data[0] == a
+    static public void push(ArrayQueueADT queue, Object a) {
+        ensureCapacity(queue, queue.size + 1);
+        queue.size++;
+        queue.head = prev(queue, queue.head);
+        queue.data[queue.head] = a;
+    }
+
     // pre: size > 0
-    // post: result == data[head]
+    //      queue != null
+    // post: result == data[0]
+    //       Any 0 <= i < size data[i] == data'[i]
     static public Object element(ArrayQueueADT queue) {
         return queue.data[queue.head];
     }
 
     // pre: size > 0
-    // post: result == data[head] && head = head' + 1
+    //      queue != null
+    // post: result == data[size - 1]
+    //       Any 0 <= i < size data[i] == data'[i]
+    static public Object peek(ArrayQueueADT queue) {
+        return queue.data[prev(queue, queue.tail)];
+    }
+
+    // pre: size > 0
+    //      queue != null
+    // post: result == data[0]
+    //       Any 0 <= i < size data[i + 1] == data'[i]
     static public Object dequeue(ArrayQueueADT queue) {
         queue.size--;
         Object tmp = queue.data[queue.head];
+        queue.data[queue.head] = null;
         queue.head = next(queue, queue.head);
         return tmp;
     }
 
+    // pre: size > 0
+    //      queue != null
+    // post: result == data[size - 1]
+    //       Any 0 <= i < size - 1 data[i] == data'[i]
+    static public Object remove(ArrayQueueADT queue) {
+        queue.size--;
+        queue.tail = prev(queue, queue.tail);
+        Object tmp = queue.data[queue.tail];
+        queue.data[queue.tail] = null;
+        return tmp;
+    }
+
+    //pre: queue != null
     // post: result == size
+    //       data == data';
+    //       size = size'
     static public int size(ArrayQueueADT queue) {
         return queue.size;
     }
 
+    //pre: queue != null
     // post: result == size > 0
+    //       data == data';
+    //       size = size'
     static public boolean isEmpty(ArrayQueueADT queue) {
         return queue.size == 0;
     }
 
-    // post: head = 0 && tail == 0 && size == 0
+    //pre: queue != null
+    // post: size == 0
+    //       data == data';
     static public void clear(ArrayQueueADT queue) {
         queue.tail = 0;
         queue.head = 0;
