aaf4d9b1eace6930f51409ad6f23681b7b4620a2
Vlad <vlad082006@yandex.ru>
2016-04-21 02:06:51 +0300

    JavaScript

--- expression.js
+++ expression.js
@@ -29,6 +29,14 @@ function binaryOperation(operation) {
     };
 }
 
+function unaryOperation(operation) {
+    return function (a) {
+        return function (x, y, z) {
+            return operation(a(x, y, z));
+        };
+    };
+}
+
 /*var add = binaryOperation((a, b) => a + b);
  var multiply = binaryOperation((a, b) => a * b);
  var subtract = binaryOperation((a, b) => a - b);
@@ -46,14 +54,31 @@ var subtract = binaryOperation(function (a, b) {
 var divide = binaryOperation(function (a, b) {
     return a / b;
 });
+var mod = binaryOperation(function (a, b) {
+    return a % b;
+});
+var power = binaryOperation(function (a, b) {
+    return Math.pow(a, b);
+});
+
+
+var negate = unaryOperation(function (a) {
+    return -a;
+});
+var abs = unaryOperation(function (a) {
+    return Math.abs(a);
+});
+var log = unaryOperation(function (a) {
+    return Math.log(a);
+});
 
 function parse(text) {
     var tokens = text.split(" ");
     var stack = [];
     tokens.forEach(function (token) {
-        if (token[0] >= '0' && token[0] <= '9') {
+        if (token[0] >= '0' && token[0] <= '9' || (token[0] === '-' && token[1] >= '0' && token[1] <= '9')) {
             stack.push(cnst(parseInt(token)));
-        } else if (token === 'x') {
+        } else if (token >= "x" && token <= "z") {
             stack.push(variable(token));
         } else {
             switch (token) {
@@ -72,6 +97,26 @@ function parse(text) {
                 case "-":
                     tmp = stack.pop();
                     stack.push(subtract(stack.pop(), tmp));
+                    break;
+                case "negate":
+                    tmp = stack.pop();
+                    stack.push(negate(tmp));
+                    break;
+                case "abs":
+                    tmp = stack.pop();
+                    stack.push(abs(tmp));
+                    break;
+                case "log":
+                    tmp = stack.pop();
+                    stack.push(log(tmp));
+                    break;
+                case "%":
+                    tmp = stack.pop();
+                    stack.push(mod(stack.pop(), tmp));
+                    break;
+                case "**":
+                    tmp = stack.pop();
+                    stack.push(power(stack.pop(), tmp));
                     break;
             }
         }
--- javascript/expression.js
+++ javascript/expression.js
@@ -1,21 +1,30 @@
 "use strict";
 
 function cnst(value) {
-    return function (x) {
+    return function (x, y, z) {
         return value;
     };
 }
 
 function variable(name) {
-    return function (x) {
-        return x;
+    return function (x, y, z) {
+        switch (name) {
+            case "x":
+                return x;
+            case "y":
+                return y;
+            case "z":
+                return z;
+            default:
+                return 0;
+        }
     };
 }
 
 function binaryOperation(operation) {
     return function (a, b) {
-        return function (x) {
-            return operation(a(x), b(x));
+        return function (x, y, z) {
+            return operation(a(x, y, z), b(x, y, z));
         };
     };
 }
@@ -29,9 +38,9 @@ function parse(text) {
     var tokens = text.split(" ");
     var stack = [];
     tokens.forEach(function (token) {
-        if (token[0] >= '0' && token[0] <= '9') {
+        if (token[0] >= '0' && token[0] <= '9' || (token[0] === '-' && token[1] >= '0' && token[1] <= '9')) {
             stack.push(cnst(parseInt(token)));
-        } else if (token === 'x') {
+        } else if (token >= "x" && token <= "z") {
             stack.push(variable(token));
         } else {
             switch (token) {
